bfae9944c29b892e8fd45d8349cde46684aa08cc
Dilmurod Djuraev <dilmurod.djuraev86@gmail.com>
2022-12-03 13:12:23 -0800

    Dilmurod's US5, Please check it.

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -30,3 +30,10 @@ order by 2 desc;
 select name, author, isbn
 from books
 where name = 'Head First Java';
+
+select * from books;
+
+select bc.name, count(*) from book_borrow bb inner join books b on bb.book_id = b.id
+inner join book_categories bc on b.book_category_id = bc.id
+group by name
+order by 2 desc;
